88c2f608b9cee4cd5758acbc46c1cabfc52f1adb
Santiago Sola <santiagosola94@gmail.com>
2022-09-22 22:54:32 -0300

    modificaciones a la entrega

--- main.js
+++ main.js
@@ -4,49 +4,49 @@ const app = express()
 
 const handlebars = require('express-handlebars')
 
-const {Server: HTTPServer} = require('http')
-const {Server: SocketServer} = require('socket.io')
+const { Server: HTTPServer } = require('http')
+const { Server: SocketServer } = require('socket.io')
 
 const httpServer = new HTTPServer(app)
 const io = new SocketServer(httpServer)
 
-const productos = [{title: 'Calculadora', price: 7500, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-158.png'},
-{title: 'Cuaderno', price: 3200, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/book-note-paper-school-158.png'},
-{title: 'Lapiz', price: 400, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/pencil-pen-stationery-school-158.png'}]
+const productos = [{ title: 'Calculadora', price: 7500, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-158.png' },
+{ title: 'Cuaderno', price: 3200, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/book-note-paper-school-158.png' },
+{ title: 'Lapiz', price: 400, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/pencil-pen-stationery-school-158.png' }]
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + "/public"));
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
-    layoutsDir: __dirname + '/public',
+    layoutsDir: __dirname + '/views',
     defaultLayout: 'formulario',
 }))
 
-app.set('views', './public')
+app.set('views', './views')
 app.set('view engine', 'hbs')
 
-io.on('connection', (socket)=>{
+io.on('connection', (socket) => {
     console.log('un usuario se ha conectado')
     socket.emit('listadoProductos', productos)
-    socket.on('productoAgregado', (data)=>{
+    socket.on('productoAgregado', (data) => {
         productos.push(data)
         io.sockets.emit('listadoProductos', productos)
     })
     socket.emit('mensajes', leerMensaje())
-    socket.on('mensajeEnviado', (data)=>{
-        const leer = fs.readFileSync('./data/mensajes.json')
+    socket.on('mensajeEnviado', (data) => {
+        const leer = fs.readFileSync('./public/db/mensajes.json')
         const parsearMensajes = JSON.parse(leer)
         parsearMensajes.push(data)
         const stringifyProductos = JSON.stringify(parsearMensajes)
-        fs.writeFileSync('./data/mensajes.json', `${stringifyProductos}`)
+        fs.writeFileSync('./public/db/mensajes.json', `${stringifyProductos}`)
         io.sockets.emit('mensajes', leerMensaje())
     })
 })
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.render('formulario', {
         layout: 'formulario',
         productosLength: productos.length
@@ -54,13 +54,13 @@ app.get('/', (req,res)=>{
 })
 
 
-function leerMensaje(){
-    const leer = fs.readFileSync('./data/mensajes.json')
+function leerMensaje() {
+    const leer = fs.readFileSync('./public/db/mensajes.json')
     const parsearMensajes = JSON.parse(leer)
     return parsearMensajes
 }
 
 
-httpServer.listen(8080, ()=>{
+httpServer.listen(8080, () => {
     console.log('Servidor Iniciado')
 })
--- public/index.js
+++ public/index.js
@@ -1,18 +1,12 @@
-const socket = io.connect()
+const socket = io()
 
-socket.on('mensaje', (data)=>{
-    console.log(data)
-})
-
-socket.on('listadoProductos', (data)=>{
-    console.log(data)
+socket.on('listadoProductos', (data) => {
     render(data)
 })
 
-socket.on('mensajes', (data)=>{
-    console.log(data)
-    const listaMensajes = data.map((mensaje)=>
-    `
+socket.on('mensajes', (data) => {
+    const listaMensajes = data.map((mensaje) =>
+        `
         <li>
             <span style="color:blue"><b>${mensaje.email}</b></span> 
             <span style="color:red">[${mensaje.fecha}] :</span>
@@ -24,8 +18,8 @@ socket.on('mensajes', (data)=>{
 
 
 
-function render(data){
-    const ArrayProductos = data.map((producto)=>
+function render(data) {
+    const ArrayProductos = data.map((producto) =>
         `<tr>
             <td>${producto.title}</td>
             <td>$${producto.price}</td>
@@ -35,25 +29,28 @@ function render(data){
     document.getElementById('cuerpoTabla').innerHTML = ArrayProductos
 }
 
-function crearProducto(){
+function crearProducto() {
     const titulo = document.getElementById('title').value
     const precio = document.getElementById('price').value
     const thumb = document.getElementById('thumbnail').value
 
-    socket.emit('productoAgregado', {title: titulo, price: precio, thumbnail: thumb})
+    socket.emit('productoAgregado', { title: titulo, price: precio, thumbnail: thumb })
     return false;
 }
 
-function mensajeEnviado(){
+function mensajeEnviado() {
     const emailUsuario = document.getElementById('emailUsuario').value
     const mensajeUsuario = document.getElementById('mensajeUsuario').value
     document.getElementById('mensajeUsuario').value = ""
     const fecha = new Date()
 
-    if(emailUsuario != "") {
-        socket.emit('mensajeEnviado', {email: emailUsuario, fecha, msj: mensajeUsuario})
+    if (emailUsuario != "") {
+        socket.emit('mensajeEnviado', { email: emailUsuario, fecha, msj: mensajeUsuario })
         document.getElementById('emailIncorrecto').innerHTML = ""
     } else {
         document.getElementById('emailIncorrecto').innerHTML = '<div class="alert alert-warning">¡Ingrese un email!</div>'
     }
 }
+
+
+
